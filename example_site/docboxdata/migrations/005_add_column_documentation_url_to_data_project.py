c47d2d9089ffc33b9e92afb31a3a60de5872a988
Stefan Gabriels <stefan@macbook-pro-van-stefan-gabriels-2.local>
2009-01-27 11:47:39 +0100

    reverted migrations to original

--- example_site/docboxdata/migrations/005_add_column_documentation_url_to_data_project.py
+++
@@ -1,3 +0,0 @@
-from dmigrations.mysql import migrations as m
-import datetime
-migration = m.AddColumn('data', 'project', 'documentation_url', 'integer NULL', 'data_versionurl')
--- example_site/docboxdata/migrations/006_add_column_source_url_to_data_project.py
+++
@@ -1,3 +0,0 @@
-from dmigrations.mysql import migrations as m
-import datetime
-migration = m.AddColumn('data', 'project', 'source_url', 'integer NULL', 'data_sourceurl')
