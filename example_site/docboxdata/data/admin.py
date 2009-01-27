ac07c65223bb29f9468da13f5f0e519be76d6bc4
Stefan Gabriels <stefan@macbook-pro-van-stefan-gabriels-2.local>
2009-01-27 11:48:15 +0100

    cleaned up data model

--- example_site/docboxdata/data/admin.py
+++ example_site/docboxdata/data/admin.py
@@ -1,19 +1,7 @@
 from django.contrib import admin
-from docboxdata.data.models import Project, VersionURL, SourceURL, DocStatus
+from docboxdata.data.models import Project
 
-class VersionURLAdmin(admin.ModelAdmin):
-    pass
-
-class SourceURLAdmin(admin.ModelAdmin):
-    pass
-    
 class ProjectAdmin(admin.ModelAdmin):
     pass
-
-class DocStatusAdmin(admin.ModelAdmin):
-    pass
     
-admin.site.register(VersionURL, VersionURLAdmin)
-admin.site.register(SourceURL, SourceURLAdmin)
 admin.site.register(Project, ProjectAdmin)
-admin.site.register(DocStatus, DocStatusAdmin)
--- example_site/docboxdata/data/models.py
+++ example_site/docboxdata/data/models.py
@@ -1,18 +1,5 @@
 from django.db import models
 
-class VersionURL(models.Model):
-    url = models.CharField(max_length=200, verbose_name='Documentation URL', blank=True)
-    working_url = models.CharField(max_length=200, verbose_name='Documentation URL', blank=True)
-
-    def __unicode__(self):
-        return self.url
-
-class SourceURL(models.Model):
-    url = models.CharField(max_length=200, verbose_name='Source URL', blank=True)
-
-    def __unicode__(self):
-        return self.url
-
 class Project(models.Model):
     name = models.CharField(max_length=120)
     identifier = models.CharField(max_length=30, unique=True)
@@ -22,8 +9,6 @@ class Project(models.Model):
     homepage = models.URLField(blank=True)
     doc_url = models.CharField(max_length=200, verbose_name='Documentation URL', blank=True)
     src_url = models.CharField(max_length=200, verbose_name='Source URL', blank=True)
-    documentation_url = models.ForeignKey(VersionURL, null=True)
-    source_url = models.ForeignKey(SourceURL, null=True)
     
     def __unicode__(self):
         if self.name:
@@ -34,6 +19,3 @@ class Project(models.Model):
     class Meta:
         ordering = ['identifier']
         
-class DocStatus(models.Model):
-    project = models.ForeignKey(Project)
-    type = models.CharField(max_length=2, choices=(('C', 'Create'), ('U', 'Update')))
