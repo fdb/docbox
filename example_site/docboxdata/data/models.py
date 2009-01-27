ae86247cddb80d9a7177d1f2976d3b8bb2826a1b
Stefan Gabriels <stefan@macbook-pro-van-stefan-gabriels-2.local>
2009-01-27 18:03:30 +0100

    Added some utility functions to the data model

--- example_site/docboxdata/data/models.py
+++ example_site/docboxdata/data/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import os
+from settings import DOCBOX_DOC_ROOT
 
 class Project(models.Model):
     name = models.CharField(max_length=120)
@@ -9,6 +11,26 @@ class Project(models.Model):
     homepage = models.URLField(blank=True)
     doc_url = models.CharField(max_length=200, verbose_name='Documentation URL', blank=True)
     src_url = models.CharField(max_length=200, verbose_name='Source URL', blank=True)
+    
+    def _get_file_path(self):
+        return os.path.join(DOCBOX_DOC_ROOT, self.identifier)
+    file_path = property(_get_file_path)
+    
+    def _get_absolute_url(self):
+        return "/writer/project/%s/" % self.identifier
+    absolute_url = property(_get_absolute_url)
+    
+    def page_path(self, page):
+        return os.path.join(DOCBOX_DOC_ROOT, self.identifier, page + '.html')
+
+    def pages(self):
+        li = []
+        for root, dirs, files in os.walk(os.path.join(DOCBOX_DOC_ROOT, self.identifier)):
+            for f in files:
+                fname, ext = os.path.splitext(f)
+                if fname != "" and ext == '.html':
+                    li.append(fname)
+        return li
     
     def __unicode__(self):
         if self.name:
